5374ffc145c78622b6f0c80d0cc78ca755b0baee
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2025-10-15 15:20:04 +0700

    chore: format StockModel

--- src/main/java/nguyen/storemanagementbackend/domain/store/model/StockModel.java
+++ src/main/java/nguyen/storemanagementbackend/domain/store/model/StockModel.java
@@ -17,27 +17,27 @@ import java.util.UUID;
 public class StockModel {
 
     @Id
-        @GeneratedValue(strategy = GenerationType.UUID)
-        @Column(name = "stock_id")
-        private UUID stockId;
-    
-        @ManyToOne(optional = false)
-        @JoinColumn(name = "store_id", referencedColumnName = "store_id", nullable = false)
-        private StoreModel store;
-    
-        @Column(name = "stock_name", length = 30, nullable = false)
-        private String stockName;
-    
-        @Column(name = "stock_quantity", nullable = false)
-        private int stockQuantity;
-    
-        @Column(name = "stock_description", length = 300)
-        private String stockDescription;
-    
-        @Column(name = "updated_at")
-        private LocalDateTime updatedAt;
-    
-        @ManyToOne
-        @JoinColumn(name = "updated_by", referencedColumnName = "user_id")
-        private Users updatedBy;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "stock_id")
+    private UUID stockId;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "store_id", referencedColumnName = "store_id", nullable = false)
+    private StoreModel store;
+
+    @Column(name = "stock_name", length = 30, nullable = false)
+    private String stockName;
+
+    @Column(name = "stock_quantity", nullable = false)
+    private int stockQuantity;
+
+    @Column(name = "stock_description", length = 300)
+    private String stockDescription;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "updated_by", referencedColumnName = "user_id")
+    private Users updatedBy;
 }
