88400bab80f09a7ec98cefcb4b9f300980d3b8b1
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2025-10-22 03:04:56 +0700

    feat: create StoreInfoMapper and StoreMapper interface

---
+++ src/main/java/nguyen/storemanagementbackend/domain/store/mapper/StoreInfoMapper.java
@@ -0,0 +1,17 @@
+package nguyen.storemanagementbackend.domain.store.mapper;
+
+import nguyen.storemanagementbackend.common.dto.StoreInfoResponseBasedDto;
+import nguyen.storemanagementbackend.common.mapper.UuidMapper;
+import nguyen.storemanagementbackend.domain.address.mapper.AddressMapper;
+import nguyen.storemanagementbackend.domain.store.dto.NewStoreInfoDto;
+import nguyen.storemanagementbackend.domain.store.model.StoreInfoModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {UuidMapper.class, AddressMapper.class})
+public interface StoreInfoMapper {
+    StoreInfoResponseBasedDto toStoreInfoResponseBasedDto(StoreInfoModel storeInfoModel);
+
+    @Mapping(target = "storeInfoId", ignore = true)
+    StoreInfoModel toEntity(NewStoreInfoDto newStoreInfoDto);
+}
---
+++ src/main/java/nguyen/storemanagementbackend/domain/store/mapper/StoreMapper.java
@@ -0,0 +1,26 @@
+package nguyen.storemanagementbackend.domain.store.mapper;
+
+import nguyen.storemanagementbackend.common.dto.StoreResponseBasedDto;
+import nguyen.storemanagementbackend.common.mapper.UuidMapper;
+import nguyen.storemanagementbackend.domain.store.dto.DetailedStoreResponseDto;
+import nguyen.storemanagementbackend.domain.store.dto.NewStoreDto;
+import nguyen.storemanagementbackend.domain.store.model.StoreModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {UuidMapper.class, StoreInfoMapper.class})
+public interface StoreMapper {
+
+    StoreResponseBasedDto toStoreResponseBasedDto(StoreModel store);
+    DetailedStoreResponseDto toDetailedStoreResponseDto(StoreModel store);
+
+    @Mapping(target = "storeId", ignore = true)
+    @Mapping(target = "storeOwner", ignore = true)
+    @Mapping(target = "staff", ignore = true)
+    @Mapping(target = "stocks", ignore = true)
+    @Mapping(target = "slots", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "admins", ignore = true)
+    StoreModel toEntity(NewStoreDto newStoreDto);
+}
