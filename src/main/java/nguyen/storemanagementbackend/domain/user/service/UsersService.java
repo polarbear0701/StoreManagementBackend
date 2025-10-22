00a0aa93c4a578c472b6274d7290deaf939fc8be
Phan Trong Nguyen <phantnguyen2003@gmail.com>
2025-10-22 19:15:24 +0700

    feat: use MapStruct to convert entity in UsersService

--- src/main/java/nguyen/storemanagementbackend/domain/user/service/UsersService.java
+++ src/main/java/nguyen/storemanagementbackend/domain/user/service/UsersService.java
@@ -1,11 +1,9 @@
 package nguyen.storemanagementbackend.domain.user.service;
 
 import nguyen.storemanagementbackend.common.dto.UserResponseBasedDto;
-import nguyen.storemanagementbackend.common.enumeration.AuthProvider;
-import nguyen.storemanagementbackend.common.enumeration.Role;
 import nguyen.storemanagementbackend.common.exception.NoUserFoundException;
 import nguyen.storemanagementbackend.common.exception.UserAlreadyExistsException;
-import nguyen.storemanagementbackend.domain.address.model.Address;
+
 import nguyen.storemanagementbackend.domain.user.dto.RegisterRequestDto;
 import nguyen.storemanagementbackend.domain.user.mapper.UserMapper;
 import nguyen.storemanagementbackend.domain.user.model.Users;
@@ -62,29 +60,10 @@ public class UsersService {
             );
         }
 
-        Users user = Users.builder()
-                .email(requestDto.getEmail())
-                .password(passwordEncoder.encode(requestDto.getPassword()))
-                .userName(requestDto.getUserName())
-                .userAge(requestDto.getUserAge())
-                .role(Role.STAFF)
-                .authProvider(AuthProvider.LOCAL)
-                .build();
+        Users user = userMapper.toEntity(requestDto);
 
-        logger.info(user.toString());
-        if (requestDto.getAddress() != null) {
-
-            Address newAddress = Address.builder()
-                    .addressNumber(requestDto.getAddress().getAddressNumber())
-                    .addressWard(requestDto.getAddress().getAddressWard())
-                    .addressDistrict(requestDto.getAddress().getAddressDistrict())
-                    .addressCity(requestDto.getAddress().getAddressCity())
-                    .addressCountry(requestDto.getAddress().getAddressCountry())
-                    .build();
-
-            logger.info(newAddress.toString());
-            user.setAddress(newAddress);
-        }
+        user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        logger.info("New user created, id: {}", user.getUserId().toString());
         return userMapper.toUserResponseBasedDto(usersRepository.save(user));
     }
 
